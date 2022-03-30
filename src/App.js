17d75e2c997e1275cb39ac573e86c58b51399474
forhad-eng <forhadu.cse@gmail.com>
2022-03-30 18:03:10 +0600

    primarily completed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { Route, Routes } from 'react-router-dom'
 import About from './components/About/About'
 import CoinDetails from './components/CoinDetails/CoinDetails'
 import Coins from './components/Coins/Coins'
+import BdAddress from './components/Contact/BdAddress'
 import Contact from './components/Contact/Contact'
+import UsAddress from './components/Contact/UsAddress'
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import Homepage from './components/Homepage/Homepage'
@@ -16,7 +18,10 @@ const App = () => {
                 <Route path="/" element={<Homepage />} />
                 <Route path="/coins" element={<Coins />} />
                 <Route path="/coin-details/:id" element={<CoinDetails />} />
-                <Route path="/contact" element={<Contact />} />
+                <Route path="/contact" element={<Contact />}>
+                    <Route path="bd-address" element={<BdAddress />} />
+                    <Route path="us-address" element={<UsAddress />} />
+                </Route>
                 <Route path="/about" element={<About />} />
             </Routes>
             <Footer />
